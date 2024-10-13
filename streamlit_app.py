5fd19c0b13a5840d2be982a3e1617c2e04e35e56
AddonayOsoro <oaddonay@gmail.com>
2024-10-13 18:48:46 +0300

    try this

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,8 @@ from st_pages import get_nav_from_toml
 
 st.set_page_config(
     layout="wide",
-    initial_sidebar_state="collapsed" ,
+    initial_sidebar_state="collapsed",
 )
-
-
 
 
 about_page = st.Page(
@@ -47,14 +45,9 @@ home_page = st.Page(
     default=True,
 )
 
-pg = st.navigation([
-    home_page,
-    about_page,
-    login_page,
-    dash_page,
-    calc_page,
-    success_page
-])
+pg = st.navigation(
+    [home_page, about_page, login_page, dash_page, calc_page, success_page]
+)
 st.logo("https://cdn.usegalileo.ai/sdxl10/101f5617-1def-49ab-9188-c2575daeb32f.png")
 st.sidebar.text("Made with 💙 by Group 12")
 
